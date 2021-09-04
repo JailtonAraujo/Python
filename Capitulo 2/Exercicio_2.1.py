6665e449d6ac3669205bdab454e58058a598346e
Jailton de Araújo Santos <jailtonraneimoney@gmail.com>
2021-09-03 23:56:17 -0300

    Create Exercicio_2.1.py
    Capitulo 2

---
+++ Capitulo 2/Exercicio_2.1.py
@@ -0,0 +1,16 @@
+# Exercícios do livro Introdução à Programação Com Python - 3° Edição
+# Autor: Nilo Ney Coutinho Menezes
+# Ano: 2019
+# Editora: Novatec Editora Ldta #
+
+
+# Exercício 2.1 Converta as seguintes  expressões  matemáticas  para que possam  ser calculadas usando o  interpretador Python.  #
+# 10 + 20  X  30
+# 4² ÷ 30
+# (94  + 2) X  6  -  1
+
+print(10 + (20 * 30))
+
+print(4 ** 2 // 3)
+
+print((9 ** 4 + 2) * 6 + - 1)
