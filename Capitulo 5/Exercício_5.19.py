8eac6a19e2a17583c8dace400318e2b3bd6d34a1
Jailton de Araújo Santos <jailtonraneimoney@gmail.com>
2021-09-09 20:01:32 -0300

    atualização

--- Capitulo 5/Exercício_5.19.py
+++ Capitulo 5/Exercício_5.19.py
@@ -4,14 +4,5 @@
 # Ano: 2019
 # Editora: Novatec Editora Ldta #
 
-# Exercício 5.19 Modifique o programa para aceitar valores decimais, ou seja, também contar moedas de 0,01, 0,02, 0,05, 0,10 e 0,50. 
-
-tabuada = 1 
-while tabuada <= 10:  
-    numero = 1 
-    while numero <= 10:  
-        print(f"{tabuada} x {numero} = {tabuada * numero}" ) 
-        numero += 1
-    tabuada+= 1  
-
+# Exercício 5.19 Modifique o programa para aceitar valores decimais, ou seja, também contar moedas de 0,01, 0,02, 0,05, 0,10 e 0,50.
 
