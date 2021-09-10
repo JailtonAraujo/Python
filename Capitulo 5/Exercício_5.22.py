04964ecb6333d7dd56a87449893a1e10eef20613
Jailton de Araújo Santos <jailtonraneimoney@gmail.com>
2021-09-10 20:42:17 -0300

    Adicioando

--- Capitulo 5/Exercício_5.22.py
+++ Capitulo 5/Exercício_5.22.py
@@ -7,3 +7,41 @@
 #Exercício 5.22 Escreva um programa que exiba uma lista de opções (menu): adição,
 #subtração, divisão, multiplicação e sair. Imprima a tabuada da operação escolhida.
 #Repita até que a opção saída seja escolhida.
+
+
+while True:
+    print(30 * "=")
+    print("{:^30}".format("TABUADA"))
+    print(30*"=")
+    print("[1] ADIÇÃO\n"
+          "[2] SUBTRAÇÃO\n"
+          "[3] DIVISÃO\n"
+          "[4] MULTIPLICAÇÃO\n"
+          "[5] PARA SAIR")
+    opcao = int(input("ESCOLA UMA DAS OPÇÕES ACIMA\n>>>"))
+
+    if opcao == 5:
+        print("Finalizado com sucesso!")
+        break
+    elif opcao >= 1 and opcao < 5:
+        num = int(input("Digite o numero para saber a tabuada:"))
+        i =1
+
+        if opcao == 1:
+            while i <= 10:
+                print(f"{num} + {i} = {num+i}")
+                i += 1
+        elif opcao == 2:
+            while i <= 10:
+                print(f"{num} - {i} = {num-i}")
+                i += 1
+        elif opcao == 3:
+            while i <= 10:
+                print(f"{num} ÷ {i} = {num/i:5.2f}")
+                i += 1
+        elif opcao == 4:
+            while i <= 10:
+                print(f"{num} X {i} = {num*i:5.2f}")
+                i += 1
+    else:
+        print("Opção invalida!")
---
+++ Capitulo 5/Exercício_5.23.py
@@ -0,0 +1,12 @@
+# coding=utf-8
+# Exercícios do livro Introdução à Programação Com Python - 3° Edição
+# Autor: Nilo Ney Coutinho Menezes
+# Ano: 2019
+# Editora: Novatec Editora Ldta #
+
+# Exercício 5.23 Escreva um programa que leia um número e verifique se é ou não
+# um número primo. Para fazer essa verificação, calcule o resto da divisão do número
+# por 2 e depois por todos os números ímpares até o número lido. Se o resto de uma
+# dessas divisões for igual a zero, o número não é primo. Observe que O e 1 não são
+# primos e que 2 é o único número primo que é par.
+
