6b5575244847c1ffbd79664ed00ef0b04d319317
Jailton de Araújo Santos <jailtonraneimoney@gmail.com>
2021-09-21 22:27:15 -0300

    atualização

---
+++ Capitulo 6/Exercício_6.14.py
@@ -0,0 +1,7 @@
+# coding=utf-8
+# Exercícios do livro Introdução à Programação Com Python - 3° Edição
+# Autor: Nilo Ney Coutinho Menezes
+# Ano: 2019
+# Editora: Novatec Editora Ldta #
+
+#O que acontece quando a lista já está ordenada? Rastreie o Programa 6.20, mas com a lista L = [1, 2, 3, 4, S].
