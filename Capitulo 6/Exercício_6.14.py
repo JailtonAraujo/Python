604ccd310f11acc572954aa31e08eb2c0514bfed
Jailton de Araújo Santos <jailtonraneimoney@gmail.com>
2021-10-12 15:07:45 -0300

    update

--- Capitulo 6/Exercício_6.14.py
+++ Capitulo 6/Exercício_6.14.py
@@ -5,7 +5,7 @@
 # Editora: Novatec Editora Ldta #
 
 #6.14 O que acontece quando a lista já está ordenada? Rastreie o Programa 6.20, mas com a lista L = [1, 2, 3, 4, S].
-#hjhjjhjhjjj
+
 
 L = [ 7, 4, 3, 12, 8]
 
