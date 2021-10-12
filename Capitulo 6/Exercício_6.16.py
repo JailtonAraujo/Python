838d30f720d8c890cdbbfff1ea4dfa8802eb3a6d
Jailton de Araújo Santos <jailtonraneimoney@gmail.com>
2021-10-12 15:19:57 -0300

    Update

---
+++ Capitulo 6/Exercício_6.16.py
@@ -0,0 +1,8 @@
+# coding=utf-8
+# Exercícios do livro Introdução à Programação Com Python - 3° Edição
+# Autor: Nilo Ney Coutinho Menezes
+# Ano: 2019
+# Editora: Novatec Editora Ldta #
+
+#Exercício 6.16 Modifique o Programa 6.20 para ordenar a lista em ordem decrescente.
+# L = [1, 2, 3, 4, S] deve ser ordenada como L = [S, 4, 3, 2, 1].
