da60f3afc930399b9ae1fab7d40afc0dc6cc2cdf
Jailton de Araújo Santos <jailtonraneimoney@gmail.com>
2021-10-15 14:37:23 -0300

    atualização

--- Capitulo 6/Exercício_6.18.py
+++ Capitulo 6/Exercício_6.18.py
@@ -1,5 +1,19 @@
-estoque= {"tomate": [1000, 2.30],
-"alface": [500, 0.45],
-"batata": [2001, 1.20],
-"feijão": [100, 1.50]}
+# coding=utf-8
+# Exercícios do livro Introdução à Programação Com Python - 3° Edição
+# Autor: Nilo Ney Coutinho Menezes
+# Ano: 2019
+# Editora: Novatec Editora Ldta #
 
+#Exercício 6.18 Escreva um programa que gere um dicionário, em que cada chave seja
+#um caractere, e seu valor seja o número desse caractere encontrado em uma frase lida.
+#Exemplo: O rato -> {"O":1, "r":1, "a":1, "t":1, "o":1}
+
+d = {}
+
+for letra in "Orato":
+    if letra in d:
+        d[letra] = d[letra]
+    else:
+        d[letra] = 1
+
+print(d)
