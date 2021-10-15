ec4f6d81a2cc6450f48e633ac37128ef713077b6
Jailton de Araújo Santos <jailtonraneimoney@gmail.com>
2021-10-15 14:40:22 -0300

    atualização

--- Capitulo 6/Exercício_6.19.py
+++ Capitulo 6/Exercício_6.19.py
@@ -4,7 +4,7 @@
 # Ano: 2019
 # Editora: Novatec Editora Ldta #
 
-#Exercício 6.19 Escreva um programa que compare duas listas. Utilizando operações com conjuntos, imprima:
+#Exercício 6.19 Escreva um programa que compare duas listas. Utilizando operações com conjuntos, imprima:
 #   • os valores comuns às duas listas
 #   • os valores que só existem na primeira
 #   • os valores que existem apenas na segunda
