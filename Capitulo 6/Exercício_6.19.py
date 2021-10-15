1e2ec9ee0efd1074d9f8dc48ca8c12c435298410
Jailton de Araújo Santos <jailtonraneimoney@gmail.com>
2021-10-15 14:40:06 -0300

    atualização

--- Capitulo 6/Exercício_6.19.py
+++ Capitulo 6/Exercício_6.19.py
@@ -3,3 +3,10 @@
 # Autor: Nilo Ney Coutinho Menezes
 # Ano: 2019
 # Editora: Novatec Editora Ldta #
+
+#Exercício 6.19 Escreva um programa que compare duas listas. Utilizando operações com conjuntos, imprima:
+#   • os valores comuns às duas listas
+#   • os valores que só existem na primeira
+#   • os valores que existem apenas na segunda
+#   • uma lista com os elementos não repetidos das duas listas.
+#   • a primeira lista sem os elementos repetidos na segunda
