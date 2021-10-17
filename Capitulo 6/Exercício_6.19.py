fbc2d5eb150c998e2dd7e8e1176a367bf0d83907
Jailton de Araújo Santos <jailtonraneimoney@gmail.com>
2021-10-17 00:27:13 -0300

    uptade

--- Capitulo 6/Exercício_6.19.py
+++ Capitulo 6/Exercício_6.19.py
@@ -10,3 +10,17 @@
 #   • os valores que existem apenas na segunda
 #   • uma lista com os elementos não repetidos das duas listas.
 #   • a primeira lista sem os elementos repetidos na segunda
+
+L1 = {5,0,8,3,8,5}
+L2 = {5,7,2,8,9,0,2}
+
+print(f"Valores comuns às duas listas:{L1 & L2}")
+
+print(f"Valores que só existem na primeira lista: {L1 - L2}")
+
+print(f"valores que existem apenas na segunda lista: {L2 - L1}")
+
+
+print(f"Elementos não repetidos das duas listas: {L1 ^ L2}")
+
+print(f"Primeira lista sem os elementos repetidos na segunda: {L1 - L2}")
