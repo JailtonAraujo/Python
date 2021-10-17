0b018097f820a237735622ec8da7c7b5d731d02c
Jailton de Araújo Santos <jailtonraneimoney@gmail.com>
2021-10-17 01:06:13 -0300

    update

--- Capitulo 07/Exercício_7.01.py
+++ Capitulo 07/Exercício_7.01.py
@@ -7,3 +7,12 @@
 #Exercício 7.1 Escreva um programa que leia duas strings. Verifique se a segunda
 #ocorre dentro da primeira e imprima a posição de início.
 
+S1 = input("Digite um texto:")
+S2 = input("Digite outro texto:")
+
+if S2 in S1:
+    print("A segunda lista ocorre dentro da primeira!")
+    print(f"A posição de inicio é: {S1.rfind(S2)}")
+
+else:
+    print("A segunda lista não ocorre dentro da primeira!")
