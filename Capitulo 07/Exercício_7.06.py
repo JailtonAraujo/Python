999dc6198a41461bd934ed07640c8de8613cbf7a
Jailton de Araújo Santos <jailtonraneimoney@gmail.com>
2021-10-17 02:54:58 -0300

    update

---
+++ Capitulo 07/Exercício_7.06.py
@@ -0,0 +1,13 @@
+# coding=utf-8
+# Exercícios do livro Introdução à Programação Com Python - 3° Edição
+# Autor: Nilo Ney Coutinho Menezes
+# Ano: 2019
+# Editora: Novatec Editora Ldta #
+
+#Exercício 7.6 Escreva um programa que leia três strings. Imprima o resultado da
+#substituição na primeira, dos caracteres da segunda pelos da terceira.
+
+S1 = input("Digite a primeira String:")
+S2 = input("Digite a segunda String:")
+S3 = input("Digite a terceira String:")
+
