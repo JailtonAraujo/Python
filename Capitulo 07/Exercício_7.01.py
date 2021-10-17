8a98882262962dd73c4341016e7c72186328d5a3
Jailton de Araújo Santos <jailtonraneimoney@gmail.com>
2021-10-17 00:45:55 -0300

    Update Exercício_7.01.py

---
+++ Capitulo 07/Exercício_7.01.py
@@ -0,0 +1,9 @@
+# coding=utf-8
+# Exercícios do livro Introdução à Programação Com Python - 3° Edição
+# Autor: Nilo Ney Coutinho Menezes
+# Ano: 2019
+# Editora: Novatec Editora Ldta #
+
+#Exercício 7.1 Escreva um programa que leia duas strings. Verifique se a segunda
+#ocorre dentro da primeira e imprima a posição de início.
+
